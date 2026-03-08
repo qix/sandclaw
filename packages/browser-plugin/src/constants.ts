3fa902b60b62d36a80bf85c6fc631042bb1b8715
Josh Yudaken <j@yud.co.za>
2026-03-08 18:10:10 -0400

    Remove old muteworker and browser-plugin (pre claude agent)

--- packages/browser-plugin/src/constants.ts
+++
@@ -1,3 +0,0 @@
-export const BROWSER_VERIFICATION_ACTION = "request_research";
-export const BROWSER_CONFIDANTE_JOB_TYPE = "browser:research_request";
-export const DEFAULT_BROWSER_RESULT_JOB_TYPE = "browser:research_result";
